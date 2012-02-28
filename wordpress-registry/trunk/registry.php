6a647abebd4ebcbe236a811486005af955ee5dbe
rbncha <rbncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-28 09:40:47 +0000

    - added: readme.txt - modified: Introduced method based implementation as set/get/has Data so that you can use registry()->setObjectName(new stdObject), registry()->hasObjectName() registry()->getObjectName() etc.

--- wordpress-registry/trunk/registry.php
+++ wordpress-registry/trunk/registry.php
@@ -1,22 +1,78 @@
 <?php
 /**
  * @package Wordpress Registry
- * @version 1.0
+ * @version 2.0
  */
 /*
 Plugin Name: Wordpress Registry
 Description: Use this plugin to store anything in registry such as variable/values, objects, array etc which you can get from plugins and theme files. As the plugin runs (most probably) before any plugins is initialized. So you use it to get/store values even from plugins.
 Author: Rubin Shrestha
-Version: 1.0
+Version: 2.0
 Author URI: http://www.rubin.com.np
 */
 
 add_action( 'init', 'registry_init', -1000 );
 
 function registry_init(){
-	if( ! is_object($GLOBALS['_system_registry_']) ) $GLOBALS['_system_registry_'] = new stdClass;
+	if( ! is_object($GLOBALS['_system_registry_']) ) $GLOBALS['_system_registry_'] = new systemRegistry();
 }
 
 function registry(){
 	return $GLOBALS['_system_registry_'];
+}
+
+
+class systemRegistry
+{
+	private $_data = array();
+	
+	public function __construct(){ }
+	
+	public function getData($key = null, $default = null)
+    {
+		if (null === $key) {
+			return $this->_data;
+		}
+
+		return isset($this->_data[$key]) ? $this->_data[$key] : $default;
+    }
+
+    public function setData($key, $value)
+    {
+        $this->_data[$key] = $value;
+        return $this;
+    }
+	
+	public function hasData($key)
+	{
+		return (bool) isset($this->_data[$key]);
+	}
+	
+	public function __get($key)
+    {
+        return $this->getData($key);
+    }
+
+    public function __set($key, $value)
+    {
+        $this->_data[$key] = $value;
+    }
+
+    public function __call($name, $arguments)
+    {
+		$key = substr($name, 3);
+		$key[0] = strtolower($key[0]);
+		
+		switch (substr($name, 0, 3)) {
+			case 'has':
+				return $this->hasData($key);
+			case 'get':
+				return $this->getData($key);
+			case 'set':
+				$this->setData($key, $arguments[0]);
+				return $this;
+		}
+
+		throw new Exception("Call to undefined method " . get_class($this) . '::' . $name);
+    }
 }
